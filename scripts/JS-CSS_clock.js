724fb5f3a42a775e1c25f95041661abb849a00fc
Tamara A <akira-49-q@yandex.ru>
2020-07-15 17:52:19 +0300

    feat: изменить вид часов на странице JS-CSS Clock

---
+++ scripts/JS-CSS_clock.js
@@ -0,0 +1,29 @@
+const clockHand = document.querySelector('.clock__hand');
+const secondHand = document.querySelector('.clock__hand_style_second');
+const minuteHand = document.querySelector('.clock__hand_style_min');
+const hourHand = document.querySelector('.clock__hand_style_hour');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  if (secondHand.style.transform == '444deg') {
+    clockHand.style.transition = 'all 0s';
+  } else  if (secondHand.style.transform == '0deg') {
+    clockHand.style.transition = 'all .05s';
+  }
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
